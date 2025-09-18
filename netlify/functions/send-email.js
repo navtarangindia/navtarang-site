da7e76f2c06797db0a3f5bdcce060bfc789fcf29
Navtarang India <hello@navtarangindia.com>
2025-09-18 12:29:02 +0530

    Debug logs for send-email function

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -45,7 +45,7 @@ exports.handler = async (event) => {
     const clientId = process.env.AMAZON_CLIENT_ID; // safer than hardcoding
     const redirectUri = "https://navtarangindia.com/netlify/functions/callback";
 
-    const lwaUrl = `https://www.amazon.com/ap/oa?client_id=${clientId}&scope=advertising::campaign_management&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&state=USER_${userId}`;
+    const lwaUrl = `https://www.amazon.com/ap/oa?client_id=client_id=amzn1.application-oa2-client.d9273e4359b94f1b892bbf2bca64e300&scope=advertising::campaign_management&response_type=code&redirect_uri=https://navtarangindia.com/netlify/functions/callback&state=USER_${userId}`;
 
     // 5️⃣ Send email with LWA button
     let mailOptions = {
